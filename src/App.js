43fc5a809b4b5ac04d9dec5b51365f421f3060fd
Guilherme Bastos <gbastos127@gmail.com>
2020-07-28 23:41:28 -0300

    setup da aplicacao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Menu from "./components/Menu";
+import Carousel from "./components/Carousel";
+import dadosInicial from "./data/dados_iniciais.json";
+import BannerMain from "./components/BannerMain";
 
 function App() {
   return (
-    <nav>
+    <div className="App">
       <Menu />
-    </nav>
+      <BannerMain
+        videoTitle={dadosInicial.categorias[0].videos[0].titulo}
+        videoDescription="O meliante tava de boa e tomo a rajada flamenjando to tiozão-san!"
+        url={dadosInicial.categorias[0].videos[0].url}
+      />
+      <Carousel ignoreFirstVideo category={dadosInicial.categorias[0]} />
+      <Carousel category={dadosInicial.categorias[1]} />
+      <Carousel category={dadosInicial.categorias[2]} />
+      <Carousel category={dadosInicial.categorias[3]} />
+      <Carousel category={dadosInicial.categorias[4]} />
+      <Carousel category={dadosInicial.categorias[5]} />
+    </div>
   );
 }
 
