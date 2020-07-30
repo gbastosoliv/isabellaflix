ac9a48f331a6a8944a0d6347192a390ad10492bd
Guilherme Bastos <gbastos127@gmail.com>
2020-07-30 01:07:07 -0300

    configuracao roteamento e state

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Menu from "./components/Menu";
-import Carousel from "./components/Carousel";
-import dadosInicial from "./data/dados_iniciais.json";
-import BannerMain from "./components/BannerMain";
+import Menu from "../../components/Menu";
+import Carousel from "../../components/Carousel";
+import dadosInicial from "../../data/dados_iniciais.json";
+import BannerMain from "../../components/BannerMain";
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <Menu />
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
